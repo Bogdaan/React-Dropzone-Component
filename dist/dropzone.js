a5bbffc5e0f930aa7e5b7b793793992589cd4d1e
Novikov Bogdan <hcbogdan@gmail.com>
2016-03-29 14:17:36 +0300

    global react

--- dist/dropzone.js
+++ dist/dropzone.js
@@ -1,12 +1,12 @@
 /*!
- * react-dropzone-component 0.9.0 - 
+ * react-dropzone-component 0.9.0 -
  * MIT Licensed
  */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.ReactDropzone = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-var React = (typeof window !== "undefined" ? window['React'] : typeof global !== "undefined" ? global['React'] : null),
-    ReactDOM = (typeof window !== "undefined" ? window['ReactDOM'] : typeof global !== "undefined" ? global['ReactDOM'] : null),
+var React = (typeof window !== "undefined" ? window.React : typeof global !== "undefined" ? global['React'] : null),
+    ReactDOM = (typeof window !== "undefined" ? window.ReactDOM : typeof global !== "undefined" ? global['ReactDOM'] : null),
     Helpers = require('./helpers'),
     IconComponent = require('./icon'),
     Dropzone, DropzoneComponent;
@@ -131,15 +131,15 @@ DropzoneComponent = React.createClass({displayName: "DropzoneComponent",
 
         if (!this.props.config.postUrl && this.props.action) {
             return (
-                React.createElement("form", {action: this.props.action, className: className}, 
-                    icons, 
+                React.createElement("form", {action: this.props.action, className: className},
+                    icons,
                     this.props.children
                 )
             );
         } else {
             return (
-                React.createElement("div", {className: className}, 
-                    icons, 
+                React.createElement("div", {className: className},
+                    icons,
                     this.props.children
                 )
             );
@@ -2912,7 +2912,7 @@ module.exports = Helpers;
 'use strict';
 
 var React = (typeof window !== "undefined" ? window['React'] : typeof global !== "undefined" ? global['React'] : null);
-    
+
 var Icon = React.createClass({displayName: "Icon",
     render: function () {
         return (
